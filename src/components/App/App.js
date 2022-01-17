538945317e6ad27bc559f0fc010c645e00cb0cf7
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2022-01-17 17:57:12 +0200

    added saving to localStorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -49,6 +49,19 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const visibleContacts = this.getVisibleContacts();
 
